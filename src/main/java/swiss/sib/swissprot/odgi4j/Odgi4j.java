30237bf934e572b804729701211c9c635d695c43
Jerven bolleman <jerven.bolleman@sib.swiss>
2022-06-22 00:05:36 +0200

    We can get a copy of the c_string_version into JNA

--- src/main/java/swiss/sib/swissprot/odgi4j/Odgi4j.java
+++ src/main/java/swiss/sib/swissprot/odgi4j/Odgi4j.java
@@ -3,10 +3,11 @@ package swiss.sib.swissprot.odgi4j;
 import com.sun.jna.Library;
 import com.sun.jna.Native;
 import com.sun.jna.NativeLong;
+import com.sun.jna.Pointer;
 
 public interface Odgi4j extends Library {
 
-	public String odgi_version();
+	public Pointer odgi_c_version();
 	
 	public NativeLong odgi_long_long_size();
 	
--- src/test/java/swiss/sib/swissprot/odgi4j/Odgi4jTest.java
+++ src/test/java/swiss/sib/swissprot/odgi4j/Odgi4jTest.java
@@ -2,16 +2,21 @@ package swiss.sib.swissprot.odgi4j;
 
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+
+import com.sun.jna.Pointer;
 
 public class Odgi4jTest {
 
 	@Test
 	public void testVersionNotNull() {
-		System.setProperty("jna.debug_load", "true");
+		System.setProperty("jna.debug_load", "false");
 		Odgi4j instance = Odgi4j.instance();
 		assertNotNull(instance);
-		assertNotEquals(0, instance.divsufsort_version());
+		Pointer versionPointer = instance.odgi_c_version();
+		String version = versionPointer.getString(0);
+		assertTrue(version, version.startsWith("v"));
 	}
 }
